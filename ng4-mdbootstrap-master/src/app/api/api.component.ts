5d91048aa2e64019177a45a474fe09d95de36624
Scazi <kevin-davis@hotmail.com>
2018-06-04 02:26:19 +0200

    Angular api integration almost done

--- ng4-mdbootstrap-master/src/app/api/api.component.ts
+++ ng4-mdbootstrap-master/src/app/api/api.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MarvelMoviesService, Movie } from '../services/marvel-movies.service';
 
 @Component({
   selector: 'app-api',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class APIComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _mms: MarvelMoviesService) { }
+
+  movie: Movie;
+  movies: Movie[];
+  maxAmount : number = 100;
+  pageNumber  : number = 0;
 
   ngOnInit() {
+    this._mms.getMovies()
+    .subscribe(result => {this.movies = result.data; this.maxAmount = result.length;})
+
+
+    this.movie = {
+      title: "",
+      director: null,
+      release: 1900,
+      score: 0
+    }
+    this.Update();
   }
 
+
+  Update = () => {
+    this._mms.getMovies()
+    .subscribe(result => {this.movies = result.data; this.maxAmount = result.length;})
+  }
+
+  btnClickAdd(){
+    this._mms.postMovie(this.movie).subscribe(result => console.log(result))
+  }
+
+  btnClickRemove(){
+    this._mms.deleteMovie(this.movie).subscribe(result => console.log(result))
+  }
+
+  btnClickUpdate(){
+    this._mms.updateMovie(this.movie).subscribe(result => console.log(result))
+  }
 }
